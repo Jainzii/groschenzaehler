8fb25720d57ad3a55a0505524ec6a3a531171e66
Jens Lippert <jens-lippert01@web.de>
2023-07-09 21:23:50 +0200

    added Blynk, board now directly sends totalAmount to soundprocessor.ino

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # groschenzaehler
 SoftSkills Sommerprojekt
+Unser Projekt besitzt zwei ESP32 Boards. Auf einem dieser Boards läuft die groschenzaehler.ino und auf dem anderen läuft die soundprocessor.ino.
+
+Die groschenzaehler.ino verarbeitet die Inputs der Laser, zählt die Menge an eingeworfenen Münzen und ist auch zuständig für die Kommunikation mit Blynk.
+
+Die soundprozessor.ino ist zuständig für den Display und den Lautsprecher und bekommt dafür von dem anderen Board die benötigten Daten.
--- groschenzaehler.ino
+++ groschenzaehler.ino
@@ -9,6 +9,22 @@
 #define MAX_DISTANCE 20
 #define NUM_LAZR 8
 
+/* Fill-in information from Blynk Device Info here */
+#define BLYNK_TEMPLATE_ID "TMPL4zQckm-w9"
+#define BLYNK_TEMPLATE_NAME "Sommerprojekt 07"
+#define BLYNK_AUTH_TOKEN "uWHTXzitvEQQiPCWWlOe7e1aywhxUhYZ"
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "YourNetworkName";
+char pass[] = "YourPassword";
 
 long lastActivation[NUM_LAZR] = { 0, 0, 0, 0, 0, 0, 0, 0 };
 long money[8] = {0,0,0,0,0,0,0,0};
@@ -17,9 +33,11 @@ int lazr[NUM_LAZR] = { LAZR1, LAZR2, LAZR3, LAZR4, LAZR5, LAZR6, LAZR7, LAZR8 };
 
 void setup() {
   Serial.begin(9600);
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
 }
 
 void loop() {
+  Blynk.run();
   sense();
 }
 
@@ -31,7 +49,10 @@ void sense() {
       if (digitalRead(lazr[i]) == 0) {
           money[i] += 1;
           lastActivation[i] = millis();
-          Serial.write(i);
+          long totalAmount = calcMoney();
+          double totalAmountAsDecimal = totalAmount / 100;
+          Blynk.virtualWrite(V4, totalAmountAsDecimal);
+          Serial.write(totalAmount);
       }
     }
   }
