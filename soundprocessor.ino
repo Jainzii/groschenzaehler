207c9e3f487ffbed033fcdb2fdb34514e5518319
Hendrik1768 <129654275+Hendrik1768@users.noreply.github.com>
2023-07-27 23:33:57 +0200

    Update soundprocessor.ino
    Some Comments...

--- soundprocessor.ino
+++ soundprocessor.ino
@@ -1,9 +1,9 @@
 #include <LiquidCrystal.h>
 
-#define speaker 12
+#define speaker 12 //speaker pin
 
 const int rs = 10, en = 11, d0 = 2, d1 = 3, d2 = 4, d3 = 5, d4 = 6, d5 = 7, d6 = 8, d7 = 9;
-LiquidCrystal lcd(rs, en, d0, d1, d2, d3, d4, d5, d6, d7);
+LiquidCrystal lcd(rs, en, d0, d1, d2, d3, d4, d5, d6, d7); //initialize LCD
 
 int count = 0;
 int waitingSounds = 0;
@@ -30,7 +30,7 @@ void setup() {
 }
 
 void loop() {
-  if (Serial.available()) {
+  if (Serial.available()) {  //check the serial port as often as possible
     if (millis() > 5000 || bugfix) {
       bugfix = true;
       // amount of cent is read
@@ -43,7 +43,7 @@ void loop() {
         moneyAmount += calcCoin(moneyTmp);
         waitingSounds += 1;
       }
-      currentTier = dropTier(moneyAmount);  // string currentTier gets set accpoding to the current tier string o.O
+      currentTier = dropTier(moneyAmount);  // string currentTier gets set according to the current tier string o.O
       lcd.clear();
       lcd.setCursor(0, 0);
       lcd.print(currentTier);
@@ -53,17 +53,17 @@ void loop() {
       lcd.print(" Euro");
     }
   }
-  playsound();
+  playsound();  //"async" playsound
 }
 
-
+//plays sounds if a coin was tossed in and if a tier upgrade happened
 void playsound() {
   if (lastTier == currentTier && waitingSounds > 0) {
-    if (!block0[0] && (millis() - last >= 500)) {
+    if (!block0[0] && (millis() - last >= 500)) {  //checks if the last activation was at least 500ms ago
       block0[0] = true;
       tone(speaker, 988, 100);
       last = millis();
-    } else if (millis() - last >= 100 && (waitingSounds > 0) && !block0[1] && block0[0]) {
+    } else if (millis() - last >= 100 && (waitingSounds > 0) && !block0[1] && block0[0]) { //blocking is necessary to ensure the Sound is played back correctly
       block0[1] = true;
       tone(speaker, 1319, 250);
     } else if (millis() - last >= 350 && (waitingSounds > 0) && !block0[2] && block0[1]) {
@@ -110,6 +110,8 @@ void playsound() {
     }
   }
 }
+
+//helper function. Converts the received integer to the according money value
 uint32_t calcCoin(int i) {
   uint32_t out;
   switch (i) {
