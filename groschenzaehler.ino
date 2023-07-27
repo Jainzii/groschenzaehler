a3f83d4b07bb57068b485dc0aa9e017c87d9a2a0
Hendrik1768 <129654275+Hendrik1768@users.noreply.github.com>
2023-07-27 23:27:40 +0200

    Update groschenzaehler.ino
    Some comments...

--- groschenzaehler.ino
+++ groschenzaehler.ino
@@ -1,4 +1,4 @@
-#define LAZR1 14  // Trigger and Echo Pin of Ultrasonic Sensor 21
+#define LAZR1 14  // pins of the photoresistors
 #define LAZR2 13
 #define LAZR3 17
 #define LAZR4 18
@@ -6,7 +6,6 @@
 #define LAZR6 16
 #define LAZR7 19
 #define LAZR8 15
-#define MAX_DISTANCE 20
 #define NUM_LAZR 8
 
 /* Fill-in information from Blynk Device Info here */
@@ -44,9 +43,10 @@ double totalMoney = 0.0;
 void setup() {
   Serial.begin(9600);
   Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
-  t.setInterval(500, updateBlynk);
+  t.setInterval(420, updateBlynk);
 }
 
+//loop through all essential components as fast as possible
 void loop() {
   t.run();
   sense();
@@ -54,19 +54,22 @@ void loop() {
 
 }
 
+//update the virtual pins every 420ms
 void updateBlynk(){
   Blynk.virtualWrite(V3, calcMoney() / 100.0);
   Blynk.virtualWrite(V1, dropTier());
 }
 
+//Terminal virtual pin
 BLYNK_WRITE(V2) {
   String value = param.asString();
   readTerminalInput(value);
 }
 
+//check the lightbarrier
 void sense() {
   for (int i = 0; i < 8; i++) {
-    if (millis() - lastActivation[i] > 69) {  //nice
+    if (millis() - lastActivation[i] > 69) {  //nice, ensures, that noch coins are countet multiple times
       if (digitalRead(lazr[i]) == 0) {
           money[i] += 1;
           lastActivation[i] = millis();
@@ -76,10 +79,12 @@ void sense() {
   }
 }
 
+//convert money array to cents
 long calcMoney(){
   return money[0]*1 + money[1]*2 + money[2]*5 + money[3]*10 + money[4]*20 + money[5]*50 + money[6]*100 + money[7]*200;
 }
 
+//Reset virtual pin
 BLYNK_WRITE(V0) {
   for (int i = 0; i < 8; i++){
     money[i] = 0;
@@ -87,6 +92,7 @@ BLYNK_WRITE(V0) {
   Serial.write(69);
 }
 
+//determines the current tier
 char * dropTier() {
   char* tier = "";
   if(calcMoney() >= 52900) {
@@ -103,39 +109,7 @@ char * dropTier() {
   return tier;
 }
 
-
-uint32_t calcCoin(int i) {
-  uint32_t out = 0;
-  switch (i) {
-    case 0:
-      out = 1;
-      break;
-    case 1:
-      out = 2;
-      break;
-    case 2:
-      out = 5;
-      break;
-    case 3:
-      out = 10;
-      break;
-    case 4:
-      out = 20;
-      break;
-    case 5:
-      out = 50;
-      break;
-    case 6:
-      out = 100;
-      break;
-    case 7:
-      out = 200;
-      break;
-  }
-  return out;
-}
-
-
+//Helper functions to determine what the user typed in the Terminal
 void readTerminalInput(String input) {
   int spacePos = input.indexOf(" ");
   String parameter = input.substring(spacePos + 1);
